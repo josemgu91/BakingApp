9570b69948ca184ab9e0e3e3f8df35c71e3f2f8f
José Miguel García Urrutia <josemgu91@gmail.com>
2018-02-15 12:58:04 -0500

    GetRecipesController implemented.

--- app/src/main/java/com/josemgu91/bakingapp/adapter/presentation/ui/graphical/GetRecipesController.java
+++ app/src/main/java/com/josemgu91/bakingapp/adapter/presentation/ui/graphical/GetRecipesController.java
@@ -1,8 +1,28 @@
 package com.josemgu91.bakingapp.adapter.presentation.ui.graphical;
 
+import com.josemgu91.bakingapp.domain.datagateways.RecipeDataGateway;
+import com.josemgu91.bakingapp.domain.usecases.GetRecipes;
+import com.josemgu91.bakingapp.domain.usecases.RecipeOutput;
+import com.josemgu91.bakingapp.domain.usecases.common.GetUseCaseOutput;
+
+import java.util.List;
+
 /**
  * Created by jose on 2/14/18.
  */
 
 public class GetRecipesController {
+
+    private final GetUseCaseOutput<List<RecipeOutput>> getRecipesUseCaseOutput;
+    private final RecipeDataGateway recipeDataGateway;
+
+    public GetRecipesController(GetUseCaseOutput<List<RecipeOutput>> getRecipesUseCaseOutput, RecipeDataGateway recipeDataGateway) {
+        this.getRecipesUseCaseOutput = getRecipesUseCaseOutput;
+        this.recipeDataGateway = recipeDataGateway;
+    }
+
+    public void getRecipes() {
+        new GetRecipes(getRecipesUseCaseOutput, recipeDataGateway).execute();
+    }
+
 }
