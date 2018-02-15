3e8e2fdf8adc53607399f1e53f021f79ac037535
José Miguel García Urrutia <josemgu91@gmail.com>
2018-02-15 16:39:36 -0500

    Internet permission added.

--- app/src/main/java/com/josemgu91/bakingapp/android/ui/MainActivity.java
+++ app/src/main/java/com/josemgu91/bakingapp/android/ui/MainActivity.java
@@ -1,7 +1,9 @@
-package com.josemgu91.bakingapp;
+package com.josemgu91.bakingapp.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.josemgu91.bakingapp.R;
 
 public class MainActivity extends AppCompatActivity {
 
