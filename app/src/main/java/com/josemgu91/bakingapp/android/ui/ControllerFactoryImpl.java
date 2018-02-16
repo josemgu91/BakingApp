a0a53e7edbe81d8b55555fc3a22def447ce2c673
José Miguel García Urrutia <josemgu91@gmail.com>
2018-02-16 16:50:16 -0500

    Factory implementation minor fixes.

--- app/src/main/java/com/josemgu91/bakingapp/android/ui/ControllerFactoryImpl.java
+++ app/src/main/java/com/josemgu91/bakingapp/android/ui/ControllerFactoryImpl.java
@@ -23,14 +23,12 @@ import com.josemgu91.bakingapp.data.remote.RemoteRetrofitRepository;
 
 public class ControllerFactoryImpl implements ControllerFactory {
 
-    private final Application application;
-
     private final UiThreadExecutor uiThreadExecutor;
     private final DefaultThreadPoolExecutor defaultThreadPoolExecutor;
     private final RemoteRetrofitRepository remoteRetrofitRepository;
 
     public ControllerFactoryImpl(Context context) {
-        this.application = (Application) context.getApplicationContext();
+        final Application application = (Application) context.getApplicationContext();
         this.uiThreadExecutor = application.getUiThreadExecutorInstance();
         this.defaultThreadPoolExecutor = application.getDefaultThreadPoolExecutorInstance();
         this.remoteRetrofitRepository = application.getRemoteRetrofitRepositoryInstance();
